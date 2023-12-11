40bb3aa41a3d865d79ac7842bb2937d4826f897e
jllpons <joanlluispons@gmail.com>
2023-12-11 11:19:40 +0100

    refactor: dgeapy runner script CLI conventions

--- dgeapy/dgeapy.py
+++ dgeapy/dgeapy.py
@@ -13,28 +13,30 @@ import subprocess
 def main():
 
     description = """
-dgeapy: Differential Gene Expression Analyisis in Python at different levels.
+Differential Gene Expression Analyisis in Python at different levels.
 
-usage: dgeapy.py <command> [options]
+Usage: python dgeapy.py <COMMAND> [OPTIONS]
 
-    commands:
-        dgea            differential gene expression analyisis
-        intersections   find intersections between the indexes of n files
+Commands:
+    dgea            differential gene expression analyisis
+    intersections   find intersections between the indexes of n files
 
-    options:
-        -h, --help
+Options:
+    -h, --help      show this help message and exit
     """
 
     arg_len = len(sys.argv)
     if arg_len == 1:
-        print(description)
+        print(description, file=sys.stderr)
+        print('error: missing subcommand', file=sys.stderr)
+        sys.exit(1)
 
     elif arg_len > 1:
         cmd = sys.argv[1]
         dgeapy_path = os.path.dirname(os.path.realpath(__file__))
 
         if cmd == '-h' or cmd == '--help':
-            print(description)
+            print(description, file=sys.stderr)
 
         elif cmd == 'dgea':
             subcmd = ['python', f'{dgeapy_path}/dgeapy_dgea.py',] + sys.argv[2:]
@@ -45,7 +47,9 @@ usage: dgeapy.py <command> [options]
             subprocess.run(subcmd)
 
         else:
-            print(description)
+            print(description, file=sys.stderr)
+            print(f'error: unknown subcommand "{cmd}"', file=sys.stderr)
+            sys.exit(1)
 
 
 if __name__ == '__main__':
